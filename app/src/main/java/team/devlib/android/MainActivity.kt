de0fb03d2288c308fb0fff41d0b5f424f9199eed
정승훈 <hks026naver@gmail.com>
2024-04-05 11:53:49 +0900

    chore :: retrofit 의존성 설정

--- app/src/main/java/team/devlib/android/MainActivity.kt
+++ app/src/main/java/team/devlib/android/MainActivity.kt
@@ -3,12 +3,15 @@ package team.devlib.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import team.devlib.designsystem.ui.theme.ProjectGeneratorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // ProjectGeneratorTheme {}
+            ProjectGeneratorTheme {
+
+            }
         }
     }
 }
