edd4bb9689830e741df3345fec428589c9491bf9
Tmdhoon2 <hks026naver@gmail.com>
2024-06-07 21:05:47 +0900

    fix :: 별접 기준 조회 수정

--- app/src/main/java/team/devlib/android/feature/home/HomeScreen.kt
+++ app/src/main/java/team/devlib/android/feature/home/HomeScreen.kt
@@ -155,7 +155,7 @@ internal fun SearchBar(
 enum class Type(val value: String) {
     VIEW("조회수"),
     REVIEW("후기수"),
-    STAR("별점"),
+    SCORE("별점"),
 }
 
 @Composable
@@ -215,10 +215,10 @@ private fun FilterButtons(
             )
         }
         OutlinedButton(
-            onClick = { onClick(Type.STAR) },
+            onClick = { onClick(Type.SCORE) },
             colors = ButtonDefaults.buttonColors(
                 containerColor = animateColorAsState(
-                    targetValue = if (type == Type.STAR) Color(0xFF555555)
+                    targetValue = if (type == Type.SCORE) Color(0xFF555555)
                     else Color.White,
                     label = "",
                 ).value,
@@ -229,10 +229,10 @@ private fun FilterButtons(
             ),
         ) {
             Text(
-                text = Type.STAR.value,
+                text = Type.SCORE.value,
                 style = DmsTheme.typography.body3,
                 color = animateColorAsState(
-                    targetValue = if (type == Type.STAR) Color.White
+                    targetValue = if (type == Type.SCORE) Color.White
                     else Color(0xFF555555),
                     label = "",
                 ).value,
