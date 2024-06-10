924b7a9ea869cbf2afaecf85c4279541265dbfcf
haeun3882 <haeun3882@gmail.com>
2024-06-10 16:03:04 +0900

    feat :: home

--- app/src/main/java/team/devlib/android/feature/home/HomeViewModel.kt
+++ app/src/main/java/team/devlib/android/feature/home/HomeViewModel.kt
@@ -7,19 +7,38 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import team.devlib.android.base.BaseViewModel
 import team.devlib.android.data.remote.api.BookApi
-import team.devlib.android.data.di.NetworkModule
+import team.devlib.android.data.remote.api.ReplyApi
 import team.devlib.android.data.remote.model.book.FetchBookRankingResponse
+import team.devlib.android.data.remote.model.question.CreateReplyRequest
 import javax.inject.Inject
 
 @HiltViewModel
 internal class HomeViewModel @Inject constructor(
     private val bookApi: BookApi,
+    private val replyApi: ReplyApi,
 ) : BaseViewModel<HomeState, HomeSideEffect>(HomeState()) {
 
     internal val books = mutableStateListOf<FetchBookRankingResponse.Book>()
+    var book = FetchBookRankingResponse.Book(
+        rank = 0,
+        id = 0L,
+        name = "",
+        author = "",
+        cover = "",
+        score = 0f,
+        reviewCount = 0,
+    )
 
     init {
         fetchBookRanking()
+    }
+
+    internal fun setBookId(bookId: Long) {
+        
+    }
+
+    internal fun onContentChange(content: String) = setState {
+        state.value.copy(content = content)
     }
 
     internal fun onKeywordChange(keyword: String) = setState {
@@ -55,13 +74,31 @@ internal class HomeViewModel @Inject constructor(
         }
     }
 
+    internal fun postReply() {
+        viewModelScope.launch(Dispatchers.IO) {
+            runCatching {
+                replyApi.postReply(
+                    questionId = state.value.questionId,
+                    createReplyRequest = CreateReplyRequest(
+                        content = state.value.content,
+                        bookId = state.value.bookId,
+                    )
+                )
+            }.onSuccess {
+                postSideEffect(HomeSideEffect.CreateReplySuccess)
+            }
+        }
+    }
 }
 
 internal data class HomeState(
     val keyword: String = "",
     val type: Type = Type.VIEW,
+    val bookId: Long = 0L,
+    val questionId: Long = 0L,
+    val content: String = "",
 )
 
 internal sealed interface HomeSideEffect {
-
+    data object CreateReplySuccess : HomeSideEffect
 }
