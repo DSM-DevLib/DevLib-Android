ddc97deb2329e982cf671c3448438d3a4524b0a2
haeun3882 <haeun3882@gmail.com>
2024-06-10 18:47:11 +0900

    feat ::  navigation viewmodel

--- app/src/main/java/team/devlib/android/navigation/DevlibApp.kt
+++ app/src/main/java/team/devlib/android/navigation/DevlibApp.kt
@@ -1,6 +1,7 @@
 package team.devlib.android.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
@@ -10,6 +11,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import team.devlib.android.feature.bookdetails.BookDetailsScreen
+import team.devlib.android.feature.home.HomeViewModel
 import team.devlib.android.feature.question.CreateQuestionScreen
 import team.devlib.android.feature.question.QuestionDetailsScreen
 import team.devlib.android.feature.reply.CreateReplyScreen
@@ -21,12 +23,16 @@ import team.devlib.android.feature.splash.SplashScreen
 @Composable
 internal fun DevlibApp() {
     val navController = rememberNavController()
+    val homeViewModel = hiltViewModel<HomeViewModel>()
     NavHost(
         navController = navController,
         startDestination = NavigationRoute.Auth.route,
     ) {
         auth(navController = navController)
-        main(navController = navController)
+        main(
+            navController = navController,
+            viewModel = homeViewModel
+        )
     }
 }
 
@@ -58,7 +64,10 @@ private fun NavGraphBuilder.auth(navController: NavController) {
     }
 }
 
-private fun NavGraphBuilder.main(navController: NavController) {
+private fun NavGraphBuilder.main(
+    navController: NavController,
+    viewModel: HomeViewModel,
+) {
     navigation(
         route = NavigationRoute.Main.route,
         startDestination = NavigationRoute.Main.MAIN,
@@ -83,8 +92,21 @@ private fun NavGraphBuilder.main(navController: NavController) {
             )
         }
 
-        composable(route = NavigationRoute.Main.CREATE_REPLY) {
-            CreateReplyScreen(navController = navController)
+        composable(
+            route = "${NavigationRoute.Main.CREATE_REPLY}/${NavigationRoute.Arguments.QUESTION_ID}",
+            arguments = listOf(
+                navArgument(
+                    name = "question-id",
+                    builder = { type = NavType.LongType },
+                )
+            )
+        ) {
+            val questionId = it.arguments?.getLong("question-id") ?: 0L
+            CreateReplyScreen(
+                navController = navController,
+                questionId = questionId,
+                viewModel = viewModel,
+            )
         }
 
         composable(route = NavigationRoute.Main.CREATE_QUESTION) {
@@ -108,7 +130,10 @@ private fun NavGraphBuilder.main(navController: NavController) {
         }
 
         composable(route = NavigationRoute.Main.SELECT_BOOK) {
-            SelectBookScreen(navController = navController)
+            SelectBookScreen(
+                navController = navController,
+                viewModel = viewModel,
+            )
         }
     }
 }
